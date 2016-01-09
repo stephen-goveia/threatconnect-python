3e8d968b482fcd6c0aae57bc40ee8895d35f73dc
Robert Simmons <rsimmons@threatconnect.com>
2016-01-09 08:10:56 -0500

    Move enum34 dependency to an extra that depends on python version since it is redundant for python 3.4+.

--- setup.py
+++ setup.py
@@ -18,7 +18,10 @@ setup(
     # test_suite = '',
     url='https://github.com/ThreatConnect-Inc/threatconnect-python',
     license='GPLv3',
-    install_requires=['requests', 'enum34', 'python-dateutil', 'psutil'],
+    install_requires=['requests', 'python-dateutil', 'psutil'],
+    extras_require={
+        ':python_version=="2.7"': ['enum34']
+    },
     # test_suite = '',
     use_2to3=True,
     # convert_2to3_doctests = [''],
