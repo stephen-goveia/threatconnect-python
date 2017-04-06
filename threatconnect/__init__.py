28f332c900ae4fcd3d51b7e4d4f8ac92871a957e
Bracey Summers <bsummers@threatconnect.com>
2017-04-06 14:54:33 +0000

    updating version and release notes

--- threatconnect/__init__.py
+++ threatconnect/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'ThreatConnect (support@threatconnect.com)'
-__version__ = '2.4.16'
+__version__ = '2.4.17'
 __license__ = 'Apache License, Version 2',
 __url__ = 'https://github.com/ThreatConnect-Inc/threatconnect-python'
 
