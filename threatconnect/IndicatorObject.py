47b8fa1af7c93c6695ebecbc5f43096c22c12fc6
Floyd Hightower <floyd.hightower27@gmail.com>
2017-04-06 21:47:22 -0400

    Handle multiple security labels in print method for indicators

--- threatconnect/IndicatorObject.py
+++ threatconnect/IndicatorObject.py
@@ -776,7 +776,8 @@ class IndicatorObject(object):
         printable_string += ('  {0!s:<28}: {1!s:<50}\n'.format('threat_assess_confidence',
                                                                self.threat_assess_confidence))
         printable_string += ('  {0!s:<28}: {1!s:<50}\n'.format('threat_assess_rating', self.threat_assess_rating))
-        printable_string += ('  {0!s:<28}: {1!s:<50}\n'.format('security_label', self.security_label))
+        printable_string += ('  {0!s:<28}: {1!s:<50}\n'.format('security_labels',
+                                                               ', '.join([sl.name for sl in self._security_label])))
         # printable_string += ('  {0!s:<28}: {1!s:<50}\n'.format('type', self.type))
         printable_string += ('  {0!s:<28}: {1!s:<50}\n'.format('weblink', self.weblink))
 
